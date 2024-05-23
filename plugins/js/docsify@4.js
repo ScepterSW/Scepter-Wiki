2df060fb2c1767a993fb2c780ded706df0a02f8c
unknown <peter.liu@mos.com>
2024-05-23 14:20:36 +0800

    remove waste files and plugins, clean up the project. and modify one bug about sidebar jump to top

--- plugins/js/docsify@4.js
+++ plugins/js/docsify@4.js
@@ -632,13 +632,13 @@
           autoHeader: !1,
           basePath: "",
           catchPluginErrors: !0,
-          cornerExternalLinkTarget: "_blank",
+          cornerExternalLinkTarget: "_self",
           coverpage: "",
           el: "#app",
           executeScript: null,
           ext: ".md",
           externalLinkRel: "noopener",
-          externalLinkTarget: "_blank",
+          externalLinkTarget: "_self",
           formatUpdated: "",
           ga: "",
           homepage: "README.md",
@@ -690,6 +690,7 @@
     ge = !0,
     se = 0;
   function le(e) {
+    
     if (ge) {
       for (
         var n,
@@ -715,14 +716,15 @@
         ((r = fe[ve(e, n.getAttribute("data-id"))]) &&
           r !== a &&
           (a && a.classList.remove("active"),
-          r.classList.add("active"),
-          (a = r),
-          !pe &&
-            h.classList.contains("sticky") &&
-            ((e = i.clientHeight),
-            (r = a.offsetTop + a.clientHeight + 40),
-            (a = a.offsetTop >= t.scrollTop && r <= t.scrollTop + e),
-            (i.scrollTop = a ? t.scrollTop : +r < e ? 0 : r - e))));
+          r.classList.add("active")));
+          //remove for the bug by peter
+          // (a = r),
+          // !pe &&
+          //   h.classList.contains("sticky") &&
+          //   ((e = i.clientHeight),
+          //   (r = a.offsetTop + a.clientHeight + 40),
+          //   (a = a.offsetTop >= t.scrollTop && r <= t.scrollTop + e),
+          //   (i.scrollTop = a ? t.scrollTop : +r < e ? 0 : r - e))));
     }
   }
   function ve(e, n) {
@@ -5534,9 +5536,9 @@
       (this.cacheTree = {}),
       (this.toc = []),
       (this.cacheTOC = {}),
-      (this.linkTarget = t.externalLinkTarget || "_blank"),
+      (this.linkTarget = t.externalLinkTarget || "_self"),
       (this.linkRel =
-        "_blank" === this.linkTarget ? t.externalLinkRel || "noopener" : ""),
+        "_self" === this.linkTarget ? t.externalLinkRel || "noopener" : ""),
       (this.contentBase = e.getBasePath());
     var n = this._initRenderer();
     this.heading = n.heading;
@@ -5731,8 +5733,7 @@
               t = t.config;
             return (
               (u = t.target || u),
-              (r =
-                "_blank" === u ? f.config.externalLinkRel || "noopener" : ""),
+              (r = "_self" === u ? f.config.externalLinkRel || "noopener" : ""),
               (n = a),
               R(e) || f._matchNotCompileLink(e) || t.ignore
                 ? (R(e) ||
@@ -6664,7 +6665,7 @@
                               ? "https://github.com/" + t
                               : t).replace(/^git\+/, "")) +
                             '" target="' +
-                            (r = r || "_blank") +
+                            (r = r || "_self") +
                             '" class="github-corner" aria-label="View source on Github"><svg viewBox="0 0 250 250" aria-hidden="true"><path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path><path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path><path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path></svg></a>'
                           : "")),
                     e.coverpage &&
